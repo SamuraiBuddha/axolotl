099ffad420eb6e159c1445e52a3c90e12d78aaf3
Jordan Paul Ehrig <76662139+SamuraiBuddha@users.noreply.github.com>
2025-07-26 21:16:23 -0400

    dgdfg

--- src/axolotl/utils/callbacks/__init__.py
+++ src/axolotl/utils/callbacks/__init__.py
@@ -17,7 +17,12 @@ import torch
 import torch.distributed as dist
 import wandb
 from datasets import load_dataset
-from optimum.bettertransformer import BetterTransformer
+try:
+    from optimum.bettertransformer import BetterTransformer
+    HAS_BETTERTRANSFORMER = True
+except ImportError:
+    BetterTransformer = None
+    HAS_BETTERTRANSFORMER = False
 from tqdm import tqdm
 from transformers import (
     GenerationConfig,
@@ -72,40 +77,43 @@ class EvalFirstStepCallback(
         return control
 
 
-class SaveBetterTransformerModelCallback(
-    TrainerCallback
-):  # pylint: disable=too-few-public-methods
-    """Callback to save the BetterTransformer wrapped model"""
+if HAS_BETTERTRANSFORMER:
+    class SaveBetterTransformerModelCallback(
+        TrainerCallback
+    ):  # pylint: disable=too-few-public-methods
+        """Callback to save the BetterTransformer wrapped model"""
 
-    def on_step_end(
-        self,
-        args: TrainingArguments,
-        state: TrainerState,
-        control: TrainerControl,
-        **kwargs,
-    ):
-        # Save
-        if (
-            args.save_strategy == IntervalStrategy.STEPS
-            and args.save_steps > 0
-            and state.global_step % args.save_steps == 0
+        def on_step_end(
+            self,
+            args: TrainingArguments,
+            state: TrainerState,
+            control: TrainerControl,
+            **kwargs,
         ):
-            control.should_save = True
+            # Save
+            if (
+                args.save_strategy == IntervalStrategy.STEPS
+                and args.save_steps > 0
+                and state.global_step % args.save_steps == 0
+            ):
+                control.should_save = True
 
-        if control.should_save:
-            checkpoint_folder = os.path.join(
-                args.output_dir,
-                f"{PREFIX_CHECKPOINT_DIR}-{state.global_step}",
-            )
+            if control.should_save:
+                checkpoint_folder = os.path.join(
+                    args.output_dir,
+                    f"{PREFIX_CHECKPOINT_DIR}-{state.global_step}",
+                )
 
-            model = BetterTransformer.reverse(kwargs["model"])
-            model.save_pretrained(checkpoint_folder)
-            # FIXME - need to cleanup old checkpoints
+                model = BetterTransformer.reverse(kwargs["model"])
+                model.save_pretrained(checkpoint_folder)
+                # FIXME - need to cleanup old checkpoints
 
-            # since we're saving here, we don't need the trainer loop to attempt to save too b/c
-            # the trainer will raise an exception since it can't save a BetterTransformer wrapped model
-            control.should_save = False
-        return control
+                # since we're saving here, we don't need the trainer loop to attempt to save too b/c
+                # the trainer will raise an exception since it can't save a BetterTransformer wrapped model
+                control.should_save = False
+            return control
+else:
+    SaveBetterTransformerModelCallback = None
 
 
 class GPUStatsCallback(
